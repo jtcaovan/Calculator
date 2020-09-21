4375d51666603261972a75fe7dc4515ef6365e06
Justin Caovan <jtcaovan@gmail.com>
2020-09-21 06:52:51 -0700

    Added number to display and clear functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,58 @@
-var display = 0;
+var display = document.querySelector("#display");
+const calculator = document.querySelector("calculator");
+const operators = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
+const clear = document.querySelector("#clear");
 
-function add() {
+var displayNumber = '0'
+
+function updateDisplay() {
+    display.textContent = displayNumber;
+}
+
+numbers.forEach((number) => {
+    number.addEventListener('click', number => {
+        displayNumber += number.target.value;
+        updateDisplay();
+        console.log(number.target.value);
+    });
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', operator => {
+        console.log(operator.target.value);
+    })
+});
+
+function clearDisplay() {
+    clear.addEventListener('click', (e) => {
+        displayNumber = 0;
+        updateDisplay();
+        console.log(e.target)
+    })
+}
+
+updateDisplay();
+clearDisplay();
+
+function operate() {
+// Takes an operator and 2 numbers and then calls 
+// one of the above functions on the numbers.
 
 }
 
-function subtract() {
-
+function add(a,b) {
+    return a+b;
 }
 
-function multiply() {
-
+function subtract(a,b) {
+    return a-b;
 }
 
-function divide() {
+function multiply(a,b) {
+    return a * b;
+}
 
+function divide(a,b) {
+    return a / b;
 }
